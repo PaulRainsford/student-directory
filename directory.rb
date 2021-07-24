1f77c3dcbe44d10d12b4dd443b64626c74371e8d
Paul Rainsford <paulrainsford69@gmail.co>
2021-07-24 18:30:23 +0100

    New method to remove DRY code

--- directory.rb
+++ directory.rb
@@ -30,6 +30,10 @@ def process(selection)
   end
 end
 
+def add_students(name, cohort = "november")
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -38,7 +42,8 @@ def input_students
     # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    # @students << {name: name, cohort: :november}
+    @add_students(name)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -84,7 +89,8 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    # @students << {name: name, cohort: cohort.to_sym}
+    add_students(name, cohort)
   end
   file.close
 end
